6dc3551cb3513b4ad871df1b820283bac5b93f17
Leopold Skyer <ChernovIU@gmail.ru>
2016-08-19 15:32:18 +1000

    work with XML

--- index.php
+++ index.php
@@ -1,26 +1,7 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Third Homework</title>
-</head>
-<body>
 <?php
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-?>
-</body>
-</html>
+$xmlPath = './data.xml';
+$xml = simplexml_load_file($xmlPath);
+foreach ($xml as $info) {
+    echo  $info->Country .'<br> '.$info ->City .' - '. $info->State.', '. $info-> Zip .'<br>'.$info->Street.'<br>'.$info->Name.'<br/><br>';
+}
