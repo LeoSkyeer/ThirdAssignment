78cacf258b080fc402c8f374a3a1ade55861996d
Leopold Skyer <ChernovIU@gmail.ru>
2016-08-19 15:41:23 +1000

    work with XML 2

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 
 $xmlPath = './data.xml';
 $xml = simplexml_load_file($xmlPath);
+$attrs = $xml->attributes();
+echo "Номер заказа ".$attrs['PurchaseOrderNumber'] .", ". "Дата заказа". $attrs['OrderDate'].'<br/><br/>';
 foreach ($xml as $info) {
-    echo  $info->Country .'<br> '.$info ->City .' - '. $info->State.', '. $info-> Zip .'<br>'.$info->Street.'<br>'.$info->Name.'<br/><br>';
+    echo $info->Address.'<br'. $info->Country .'<br> '.$info ->City .' - '. $info->State.', '. $info-> Zip .'<br>'.$info->Street.'<br>'.$info->Name.'<br/><br>';
 }
