d3d4b416c5d5415d683bc194d4fdfc47aac4985f
Leo_Sky <vasya_90@list.ru>
2016-08-21 13:31:46 +1000

    From home

--- index.php
+++ index.php
@@ -2,8 +2,22 @@
 
 $xmlPath = './data.xml';
 $xml = simplexml_load_file($xmlPath);
+
+
+
 $attrs = $xml->attributes();
-echo "Номер заказа ".$attrs['PurchaseOrderNumber'] .", ". "Дата заказа". $attrs['OrderDate'].'<br/><br/>';
-foreach ($xml as $info) {
-    echo $info->Address.'<br'. $info->Country .'<br> '.$info ->City .' - '. $info->State.', '. $info-> Zip .'<br>'.$info->Street.'<br>'.$info->Name.'<br/><br>';
+echo "Номер заказа: ".$attrs['PurchaseOrderNumber'] ."<br> ". "Дата заказа: ". $attrs['OrderDate'].'<br/><br/>';
+
+
+
+
+foreach ($xml->Address as $info) {
+    echo "Доставка: ".$info[source].'<br>'."Доставка(доп): ".$info[Type]."<br>"."Имя: ".$info->Name. "<br>"."Улица: ".$info->Street."<br>"."Город: ".$info->City."<br>"."Штат: ".$info->State."<br>"."Индекс: ".$info->Zip."<br>"."Улица: ".$info->Country."<br><br>";
+}
+
+echo "Дополнительные просьбы: ".$xml->DeliveryNotes.'<br><br>';
+
+foreach ($xml->Items->Item as $info) {
+    echo "Номер: ".$info[PartNumber]."<br>"."Название продукта : ".$info->ProductName."<br>"."Количество: " .$info->Quantity."<br>"."Цена: ".$info->USPrice."$".'<br>'."Коментарий: ".$info->Comment."<br>"."Дата доставки: ".$info->ShipDate ."<br>";
+    echo "<br>";
 }
